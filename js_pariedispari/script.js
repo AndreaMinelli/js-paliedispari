f2d548c5b4ea5fda5e2534f89605fbf59ae76373
AndreaMinelli <minelliandrea94@gmail.com>
2022-12-15 16:26:34 +0100

    Creata funzione numero random

--- js_pariedispari/script.js
+++ js_pariedispari/script.js
@@ -1,1 +1,20 @@
-console.log("JS OK");
+/*
+L’utente sceglie pari o dispari e inserisce un numero
+da 1 a 5 tramiite un form.
+Generiamo un numero random (sempre da 1 a 5) 
+per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari
+(usando una funzione)
+Dichiariamo chi ha vinto.
+*/
+
+//Creo funzione numeri random
+
+function getRandomNumber(min = 1, max = 5) {
+  max++;
+  const randomNumber = Math.floor(Math.random() * (max - min)) + min;
+  return randomNumber;
+}
+
+getRandomNumber();
