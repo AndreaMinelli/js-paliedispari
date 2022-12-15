1da5e2461d721f77255b3050fde6de17c7f54962
AndreaMinelli <minelliandrea94@gmail.com>
2022-12-15 15:29:50 +0100

    Creata funzione isPalindrome

--- js_palindromo/script.js
+++ js_palindromo/script.js
@@ -1,1 +1,18 @@
-console.log("JS OK");
+/*
+Chiedere all’utente di inserire una parola tramiite
+un form
+Creare una funzione per capire se la parola inserita
+è palindroma
+*/
+
+//Creo funzione verifica parola palindroma
+
+function isPalindrome(word) {
+  let result = false;
+  let reverseWord = "";
+  for (let i = word.length - 1; i >= 0; i--) {
+    reverseWord += word[i];
+  }
+  if (word === reverseWord) result = true;
+  return result;
+}
